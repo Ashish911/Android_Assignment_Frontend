1f5488c11eb3b76c234c2e25f0c01f280cdde21e
Ashish <ashishDongol32@gmail.com>
2020-02-01 15:28:10 +0545

    made layout background and added some icons

--- app/src/main/java/com/example/onlinefoodportal/MainActivity.java
+++ app/src/main/java/com/example/onlinefoodportal/MainActivity.java
@@ -1,52 +1,59 @@
 package com.example.onlinefoodportal;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     EditText etEmail, etPassword;
     Button btnLogin;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etEmail = findViewById(R.id.etEmail);
-        etPassword = findViewById(R.id.etPassword);
-        btnLogin = findViewById(R.id.btnLogin);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (TextUtils.isEmpty(etEmail.getText())) {
-                    etEmail.setError("Enter Email-Address");
-                    return;
-                } else if (TextUtils.isEmpty(etPassword.getText())) {
-                    etPassword.setError("Enter Password");
-                    return;
-                }
-                String Email, Password;
-                Email = String.valueOf(etEmail.getText());
-                Password = String.valueOf(etPassword.getText());
-                if ((Email.equals("Admin@admin.com")) && (Password.equals("Admin"))) {
-//                    Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
-//                    startActivity(intent);
-//                    SaveEmailPassword();
-                } else {
-                    Toast.makeText(MainActivity.this, "Username or Password is incorrect", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+//        etEmail = findViewById(R.id.etEmail);
+//        etPassword = findViewById(R.id.etPassword);
+//        btnLogin = findViewById(R.id.btnLogin);
+//
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (TextUtils.isEmpty(etEmail.getText())) {
+//                    etEmail.setError("Enter Email-Address");
+//                    return;
+//                } else if (TextUtils.isEmpty(etPassword.getText())) {
+//                    etPassword.setError("Enter Password");
+//                    return;
+//                }
+//                String Email, Password;
+//                Email = String.valueOf(etEmail.getText());
+//                Password = String.valueOf(etPassword.getText());
+//                if ((Email.equals("Admin@admin.com")) && (Password.equals("Admin"))) {
+////                    Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+////                    startActivity(intent);
+////                    SaveEmailPassword();
+//                } else {
+//                    Toast.makeText(MainActivity.this, "Username or Password is incorrect", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
     }
 
     private void SaveEmailPassword(){
