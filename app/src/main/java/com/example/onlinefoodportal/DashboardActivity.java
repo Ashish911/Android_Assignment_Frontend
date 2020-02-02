1b5d691e963fae0450581c6d8412063143112c74
Ashish <ashishDongol32@gmail.com>
2020-02-02 16:40:21 +0545

    icons fragments dasboard

--- app/src/main/java/com/example/onlinefoodportal/DashboardActivity.java
+++ app/src/main/java/com/example/onlinefoodportal/DashboardActivity.java
@@ -1,8 +1,17 @@
 package com.example.onlinefoodportal;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.onlinefoodportal.ui.AccountFragment;
+import com.example.onlinefoodportal.ui.FavouritesFragment;
+import com.example.onlinefoodportal.ui.HomeFragment;
+import com.example.onlinefoodportal.ui.LocationFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +19,35 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_favourites:
+                    selectedFragment = new FavouritesFragment();
+                    break;
+                case R.id.nav_nearbyLocation:
+                    selectedFragment = new LocationFragment();
+                    break;
+                case R.id.nav_Account:
+                    selectedFragment = new AccountFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, selectedFragment).commit();
+            return true;
+        }
+    };
 }
