ac2f598c1a663a3f9464b404fef747794233df36
Ashish <ashishDongol32@gmail.com>
2020-02-08 11:58:33 +0545

    Map progress2

--- app/src/main/java/com/example/onlinefoodportal/ui/LocationFragment.java
+++ app/src/main/java/com/example/onlinefoodportal/ui/LocationFragment.java
@@ -10,12 +10,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.onlinefoodportal.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LocationFragment extends Fragment {
+public class LocationFragment extends Fragment implements OnMapReadyCallback {
 
+    private GoogleMap mMap;
+    int Permission_ID = 44;
+    FusedLocationProviderClient mFusedLocationClient;
+    double Long;
+    double Lat;
 
     public LocationFragment() {
         // Required empty public constructor
@@ -29,4 +37,8 @@ public class LocationFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_location, container, false);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
 }
--- app/src/main/java/com/example/onlinefoodportal/ui/NearbyLoactionActivity.java
+++ app/src/main/java/com/example/onlinefoodportal/ui/NearbyLoactionActivity.java
@@ -4,6 +4,8 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.example.onlinefoodportal.R;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,7 +13,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class NearbyLoactionActivity extends FragmentActivity implements OnMapReadyCallback {
+public class NearbyLoactionActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
 
@@ -26,15 +28,6 @@ public class NearbyLoactionActivity extends FragmentActivity implements OnMapRea
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
