f6e17e1e5ff3835473d3c0f51e438adbf8a646ed
Ashish <ashishDongol32@gmail.com>
2020-02-13 17:06:48 +0545

    slight changes

---
+++ app/src/main/java/com/example/onlinefoodportal/adapter/RestaurantAdapter.java
@@ -0,0 +1,73 @@
+package com.example.onlinefoodportal.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.onlinefoodportal.R;
+import com.example.onlinefoodportal.model.Restaurant;
+import com.example.onlinefoodportal.url.Url;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.ItemViewHolder>{
+
+    private List<Restaurant> restaurantList;
+    private Context context;
+
+    public RestaurantAdapter(List<Restaurant> restaurantList, Context context) {
+        this.restaurantList = restaurantList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_category, null);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        final Restaurant restaurant = restaurantList.get(position);
+        Picasso.get().load(Url.base_url_image + restaurantList.get(position).getLogo()).into(holder.imgrestaurant);
+        holder.RestaurantName.setText(restaurant.getRestaurantName());
+        holder.Location.setText(restaurant.getLocation());
+        holder.Tags.setText(restaurant.getTags());
+        holder.Delivery.setText(restaurant.getDelivery());
+    }
+
+    @Override
+    public int getItemCount() {
+        return restaurantList.size();
+    }
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imgrestaurant;
+        private TextView RestaurantName;
+        private TextView Location;
+        private TextView Tags;
+        private TextView Delivery;
+        private ImageButton btnunclicked;
+        private ImageButton btnclicked;
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgrestaurant = itemView.findViewById(R.id.restaurantLogo);
+            RestaurantName = itemView.findViewById(R.id.RestaurantName);
+            Location = itemView.findViewById(R.id.RestaurantLocation);
+            Tags = itemView.findViewById(R.id.ResTag);
+            Delivery = itemView.findViewById(R.id.ResDelivery);
+            btnunclicked = itemView.findViewById(R.id.favourite_unclicked);
+            btnclicked = itemView.findViewById(R.id.favourite_clicked);
+        }
+    }
+}
