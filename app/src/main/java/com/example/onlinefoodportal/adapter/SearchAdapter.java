5be6c00a4fe0acd3237c1cfeb382abe201e9191f
Ashish <ashishDongol32@gmail.com>
2020-02-14 08:55:23 +0545

    lots of changes

---
+++ app/src/main/java/com/example/onlinefoodportal/adapter/SearchAdapter.java
@@ -0,0 +1,62 @@
+package com.example.onlinefoodportal.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.onlinefoodportal.R;
+import com.example.onlinefoodportal.model.Food;
+import com.example.onlinefoodportal.url.Url;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Food> foodList;
+
+    public SearchAdapter(Context context, List<Food> foodList) {
+        this.context = context;
+        this.foodList = foodList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_foodsection, null);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final Food food = foodList.get(position);
+        Picasso.get().load(Url.base_url_image + foodList.get(position).getFoodImage()).into(holder.Image);
+        holder.Name.setText(food.getFoodName());
+//        holder.Price.setText(food.getPrice());
+    }
+
+    @Override
+    public int getItemCount() {
+        return foodList.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView Image;
+        private TextView Name;
+        private TextView Price;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Image = itemView.findViewById(R.id.FoodPic);
+            Name = itemView.findViewById(R.id.FoodName);
+            Price = itemView.findViewById(R.id.FoodPrice);
+        }
+    }
+}
--- app/src/main/java/com/example/onlinefoodportal/api/UsersAPI.java
+++ app/src/main/java/com/example/onlinefoodportal/api/UsersAPI.java
@@ -7,8 +7,10 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 
 public interface UsersAPI {
 
@@ -18,6 +20,12 @@ public interface UsersAPI {
     @FormUrlEncoded
     @POST("user/login")
     Call<SignUpResponse> checkUser(@Field("Email") String Email, @Field("Password") String Password);
+
+    @GET("user/me")
+    Call<Users> getUserDetails(@Header("Authorization")String token);
+
+    @PUT("user/me")
+    Call<Users> UpdateDetails(@Header("Authorization")String token,@Body Users users);
 
     @POST("users/logout")
     Call<Void> logOut(@Header("Authorization") String token);
