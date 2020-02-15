5e25354059e6b7e356fbb7abcaf51a3cfd98b10e
Ashish <ashishDongol32@gmail.com>
2020-02-15 14:39:17 +0545

    instrumental testing

---
+++ app/src/androidTest/java/com/example/onlinefoodportal/LogoutInstrumentedTest.java
@@ -0,0 +1,44 @@
+package com.example.onlinefoodportal;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class LogoutInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> testRule = new ActivityTestRule<>(MainActivity.class);
+
+    private String Email = "kkk@kkk.com";
+    private String Password = "kkk";
+
+    @Test
+    public void LogoutUITest() {
+        onView(withId(R.id.emailLF))
+                .perform(typeText(Email));
+
+        closeSoftKeyboard();
+
+        onView(withId(R.id.passwordLF))
+                .perform(typeText(Password));
+
+        closeSoftKeyboard();
+
+        onView(withId(R.id.signinLF)).perform(click());
+
+        onView(withId(R.id.nav_Account)).perform(click());
+
+        onView(withId(R.id.Logout)).perform(click());
+    }
+
+}
